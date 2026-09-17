306753d643bbbe0b1337a4e92873cae30c9e8deb
Maxim <ratutuy55@gmail.com>
2026-09-17 22:11:03 +0300

    update file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,13 +3,12 @@ import styles from './App.module.css';
 import SearchBar from '../SearchBar/SearchBar';
 import fetchMovies from '../../services/movieService';
 import type { Movie } from '../../types/movie';
-import toast, { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import Loader from '../Loader/Loader';
 import MovieGrid from '../MovieGrid/MovieGrid';
 import MovieModal from '../MovieModal/MovieModal';
 import { useQuery } from '@tanstack/react-query';
-// import ReactPaginate from 'react-paginate';
 
 import ReactPaginateModule from 'react-paginate';
 import type { ReactPaginateProps } from 'react-paginate';
@@ -23,34 +22,7 @@ const ReactPaginate = (
   >
 ).default;
 
-// Далі в jsx використувуємо компонент ReactPaginate звичайним чином.
-
 function App() {
-  // const [movies, setMovies] = useState<Movie[]>([]);
-  // const [loading, setLoading] = useState<boolean>(false);
-  // const [error, setError] = useState<boolean>(false);
-  // const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
-
-  // async function handleSearch(query: string) {
-  //   setMovies([]);
-  //   setError(false);
-  //   setLoading(true);
-  //   try {
-  //     const data = await fetchMovies(query);
-  //     if (data.length === 0) {
-  //       toast.error('No movies found for your request.');
-  //       return;
-  //     }
-  //     setMovies(data);
-  //   } catch (error) {
-  //     setError(true);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // }
-  // function handleCloseModal() {
-  //   setSelectedMovie(null);
-  // }
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [page, setPage] = useState<number>(1);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
@@ -76,6 +48,8 @@ function App() {
       <div className={styles.app}>
         <Toaster position="top-right" />
         <SearchBar onSubmit={handleSearch} />
+        {isError && <ErrorMessage />}
+        {isLoading && <Loader />}
         {totalPage > 1 && !isLoading && (
           <ReactPaginate
             pageCount={totalPage}
@@ -89,8 +63,7 @@ function App() {
             previousLabel="←"
           />
         )}
-        {isError && <ErrorMessage />}
-        {isLoading && <Loader />}
+
         {movies.length > 0 && !isLoading && (
           <MovieGrid movies={movies} onSelect={setSelectedMovie} />
         )}
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './components/App/App.tsx';
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQueryClient,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
